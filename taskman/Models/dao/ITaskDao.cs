cb9f4c246505352d4699be4e2760651adfac48c4
dm84 <dmitriy.o84@ya.ru>
2014-12-08 00:24:35 +0500

    +модель и дао

---
+++ taskman/Models/dao/ITaskDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using taskman.Models.domain;
+
+namespace taskman.Models.dao
+{
+    interface ITaskDao
+    {
+        public IEnumerable<Task> list();
+        public void add();
+    }
+}
---
+++ taskman/Models/dao/TaskDaoEF.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using taskman.Models.domain;
+
+namespace taskman.Models.dao
+{
+	public class TaskDaoEF : ITaskDao
+	{
+		IEnumerable<Task> list()
+        {
+			using (TaskModelContainer context = new TaskModelContainer())
+			{
+				IQueryable<Task> tasksQuery = from task in context.Tasks select task;
+				return tasksQuery;
+			}
+		}
+
+		void add(Task task)
+		{
+			using (TaskModelContainer context = new TaskModelContainer())
+			{
+				context.Tasks.Add(task);
+				context.SaveChanges();
+			}
+		}
+	}
+}
