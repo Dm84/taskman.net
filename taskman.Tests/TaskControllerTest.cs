041147065c23c27495291923ed4a3e9e8ef542de
dm84 <dmitriy.o84@ya.ru>
2014-12-10 12:23:12 +0500

    +тесты

---
+++ taskman.Tests/TaskControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using taskman.Controllers;
+using taskman.Models.domain;
+
+using InputTask = taskman.Controllers.TaskController.FormattedTask;
+
+namespace taskman.Tests
+{
+	public class TaskControllerTest
+	{
+		TaskController taskController;
+		
+		TaskControllerTest()
+		{
+			taskController = new TaskController();
+		}
+
+		[TestMethod]
+		public void add()
+		{
+			InputTask task = new InputTask { id = 0, description = "description", completed = false, deadline = 0 };			
+			taskController.Post(task);
+
+		}
+	}
+}
---
+++ taskman.Tests/TaskServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using taskman.Models.domain;
+using taskman.Models.beans;
+using taskman.Models.service;
+
+namespace taskman.Tests
+{
+	[TestClass]
+	public class TaskServiceTest
+	{
+		TaskService serv;
+
+		public TaskServiceTest()
+		{
+			serv = BeanFactory.createService();
+		}
+
+		[TestMethod]
+		public void add()
+		{
+			Task task = new Task { id = 0, description = "test task", completed = false, deadline = DateTime.Now };
+			Task newTask = serv.add(task);
+
+			Assert.AreNotEqual(newTask.id, 0);
+		}
+
+		[TestMethod]
+		public void list()
+		{
+			IEnumerable<Task> tasks = serv.list();
+			Assert.AreNotEqual(tasks.GetEnumerator().MoveNext(), false);
+		}
+
+	}
+}
