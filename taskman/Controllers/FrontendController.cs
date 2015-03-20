cbe937142bb50810957c35f9aee4975e7a7a0758
dm84 <dmitriy.o84@ya.ru>
2015-03-20 19:56:29 +0500

    форма авторизации

--- taskman/Controllers/FrontendController.cs
+++ taskman/Controllers/FrontendController.cs
@@ -16,9 +16,50 @@ namespace taskman.Controllers
             return View();
         }
 
+		public class LoginModel
+		{
+			public string	signin_url;
+			public string	login;
+			public string[]	errors;
+			public string	login_param;
+			public string	pwd_param;
+			public string	login_placeholder;
+			public string	pwd_placeholder;
+			public string	pwd_confirm_placeholder;
+			public string	pwd_confirm_req_msg;
+			public string	pwd_len_req_msg;
+
+			public string	field_req_msg;
+			public string	signup_header;
+			public string	signin_header;
+
+			public string	signup_url;
+
+			public string	signin_btn_caption;
+			public string	signup_btn_caption;
+		}
+
 		public ActionResult Login()
 		{
-			return View();
+			//ViewData["signin_url"] = Url.Action("Login");
+			//ViewData["login"] = "";
+
+			return View(new LoginModel { 
+				signin_url = Url.Action("Login"),
+				field_req_msg = "Обязательное поле",
+				login = "",
+				login_param = "login",
+				login_placeholder = "Ваш логин",
+				pwd_placeholder = "Ваш пароль",
+				pwd_confirm_placeholder = "Ваш пароль еще раз",
+				pwd_confirm_req_msg = "Пароли должны совпадать",
+				pwd_len_req_msg = "Длина пароля должна быть не менее:",
+				pwd_param = "password",
+				signin_btn_caption = "Войти",
+				signup_btn_caption = "Зарегистрироваться",
+				signin_header = "Авторизация",
+				signup_header = "Регистрация"
+			});
 		}
 
     }
